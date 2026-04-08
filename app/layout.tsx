141c3ea67923989f1f52a41dcabc2e5ebc62f10b
Cecilie Jadach Petersen <Jadach95@hotmail.com>
2026-04-08 15:35:50 +0200

    removed styling in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar"
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 
 export const metadata: Metadata = {
   title: "Create Next App",
